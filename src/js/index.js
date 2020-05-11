683c35f8563d8023c2aa450267fec2488a9ba795
winston <winston.init@gmail.com>
2020-05-10 21:28:42 -0300

    output converter logic and animation added

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 'use strict'
 
+// sticky animation
+
 const arrow = document.querySelector('.arrow')
 const header = document.querySelector('header')
 
@@ -12,11 +14,41 @@ arrow.addEventListener('click', () => {
     header.classList.add('sticky')
 })
 
+// Output logic
 const inputCurrency = document.querySelector('#input-currency')
+const outputCurrency = document.querySelector('#output-currency')
 
-function resizeInput() {
+function calculateOutput() {
+    const newValue = inputCurrency.value * 5.48
+
+    outputCurrency.value = newValue.toFixed(2)
+}
+
+calculateOutput()
+
+inputCurrency.addEventListener('input', calculateOutput)
+
+// increase length of input according with chracters
+
+function resizeInput(e) {
     this.style.width = this.value.length + "ch"
+}
+
+const increaseOutputWidth = async () => {
+    await calculateOutput()
+    // asynchronous method
+
+    outputCurrency.style.width = outputCurrency.value.length + 'ch'
 }
 
 inputCurrency.addEventListener('input', resizeInput)
 resizeInput.call(inputCurrency)
+
+inputCurrency.addEventListener('input', increaseOutputWidth)
+
+outputCurrency.addEventListener('input', resizeInput)
+resizeInput.call(outputCurrency)
+
+// initialize input with 0
+
+inputCurrency.value = 0;
