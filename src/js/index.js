846a3b416927c3e6296378a539395226cd6b3ee7
winston <winston.init@gmail.com>
2020-05-13 19:22:32 -0300

    useless componentes removed

--- src/js/index.js
+++ src/js/index.js
@@ -2,52 +2,27 @@
 
 import stickyHeader from './animation.js'
 import interfaceUI from './ui.js'
+import main from './app.js'
 
 const animation = stickyHeader()
 const ui = interfaceUI()
+const app = main()
 
-ui.dropdownFirstCurrency()
-ui.dropdownSecondCurrency()
+ui.eventDropdown()
 
 animation.scrollWindow()
 animation.clickToScroll()
+animation.switchOrder()
 
-// Output logic
 const inputCurrency = document.querySelector('#input-currency')
-const outputCurrency = document.querySelector('#output-currency')
 
-function calculateOutput() {
-  const newValue = inputCurrency.value * 0.00012
-
-  if (newValue.toString().length >= 10) outputCurrency.value = newValue.toFixed(14)
-  else outputCurrency.value = newValue
-}
-
-calculateOutput()
-
-inputCurrency.addEventListener('input', calculateOutput)
-
-// increase length of input according with chracters
-function resizeInput(e) {
-  this.style.width = this.value.length + "ch"
-}
-
-const increaseOutputWidth = async () => {
-  await calculateOutput()
-  // asynchronous method
-
-  outputCurrency.style.width = outputCurrency.value.length + 'ch'
-}
-
-inputCurrency.addEventListener('input', resizeInput)
-resizeInput.call(inputCurrency)
-
-inputCurrency.addEventListener('input', increaseOutputWidth)
-
-outputCurrency.addEventListener('input', resizeInput)
-resizeInput.call(outputCurrency)
-
-// initialize input with 0
 inputCurrency.value = 0;
 
-// animation switch order
+app.calculateInput()
+
+inputCurrency.addEventListener('input', () => {
+  app.calculateInput()
+  ui.resizeDueChar()
+})
+
+
