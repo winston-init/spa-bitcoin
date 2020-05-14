d644c20cab140a787d15e3e81bb2959c6fd788e7
winston <winston.init@gmail.com>
2020-05-14 20:55:41 -0300

    request of Bitcoin API added

--- src/js/app.js
+++ src/js/app.js
@@ -2,13 +2,29 @@
 
 function main() {
   function calculateInput() {
+    const url = 'https://api.coindesk.com/v1/bpi/currentprice/USD.json'
+
     const inputCurrency = document.querySelector('#input-currency')
     const outputCurrency = document.querySelector('.result-content')
 
-    const output = inputCurrency.value * 0.00012
+    const xhr = new XMLHttpRequest()
 
-    outputCurrency.textContent = output
+    xhr.open('GET', url, true)
 
+    xhr.onload = function() {
+      if (this.status == 200) {
+        const currency = JSON.parse(this.responseText)
+
+        const worth = currency.bpi['USD'].rate
+
+        const output = inputCurrency.value / parseFloat(worth.replace(',', ''))
+
+        if (output.toString().length > 5) outputCurrency.textContent = output.toFixed(5)
+        else outputCurrency.textContent = output
+      }
+    }
+
+    xhr.send()
   }
 
   return {
