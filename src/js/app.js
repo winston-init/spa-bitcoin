bede046e051cc107e15660da98d20d9f35399cea
winston <winston.init@gmail.com>
2020-05-15 21:41:10 -0300

    request for BTC to other currency and promise tool added

--- src/js/app.js
+++ src/js/app.js
@@ -1,34 +1,71 @@
 'use strict'
 
 function main() {
+  function requestAPI(url, method) {
+    return new Promise((resolve, reject) => {
+      const request = new XMLHttpRequest()
+
+      request.open(url, method, true)
+
+      request.onload = () => {
+        if (request.status == 200) {
+          resolve(JSON.parse(request.responseText))
+        }
+      }
+
+      request.onerror = () => reject('Oops... Something went wrong!')
+      request.send()
+    })
+
+  }
+
   function calculateInput() {
     const currency = document.querySelector('.currency-symbol').textContent
+    const resultSymbol = document.querySelector('.result-symbol').textContent
 
-    const url = `https://api.coindesk.com/v1/bpi/currentprice/${currency}.json`
-    console.log(url)
+    let url
+
+    if (currency !== 'BTC') {
+      url = `https://api.coindesk.com/v1/bpi/currentprice/${currency}.json`
+    } else {
+      url = `https://api.coindesk.com/v1/bpi/currentprice/${resultSymbol}.json`
+    }
 
     const inputCurrency = document.querySelector('#input-currency')
     const outputCurrency = document.querySelector('.result-content')
 
-    const xhr = new XMLHttpRequest()
+    requestAPI('GET', url)
+      .then(response => {
+        let worth
 
-    xhr.open('GET', url, true)
+        if (currency !== 'BTC') {
+          worth = response.bpi[`${currency}`].rate
+        } else {
+          worth = response.bpi[`${resultSymbol}`].rate
+        }
 
-    xhr.onload = function() {
-      if (this.status == 200) {
-        const res = JSON.parse(this.responseText)
+        let output
 
-        const worth = res.bpi[`${currency}`].rate
-        console.log(currency)
+        if (currency !== 'BTC') {
+          output = inputCurrency.value / parseFloat(worth.replace(',', ''))
+        } else {
+          output = inputCurrency.value * parseFloat(worth.replace(',', ''))
+        }
 
-        const output = inputCurrency.value / parseFloat(worth.replace(',', ''))
-
-        if (output.toString().length > 7) outputCurrency.textContent = output.toFixed(7)
-        else outputCurrency.textContent = output
-      }
-    }
-
-    xhr.send()
+        if (currency !== 'BTC') {
+          if (output.toString().length > 7) outputCurrency.textContent = output.toFixed(7)
+          else outputCurrency.textContent = output
+        } else {
+          if (output === 0) {
+            outputCurrency.textContent = output
+          } else {
+            outputCurrency.textContent = output.toFixed(2)
+          }
+        }
+      })
+      .catch(error => {
+        console.error(error)
+      })
   }
 
   return {
