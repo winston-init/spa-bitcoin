e1363056e250084ed4ecea6ac898acfe2fdf6d7a
winston <winston.init@gmail.com>
2020-05-16 21:03:18 -0300

    strategy design pattern implementated

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
 'use strict'
 
+import { currencyStrategy, cryptocurrency, nonCryptocurrency } from './strategy.js'
+
 function main() {
   function requestAPI(url, method) {
     return new Promise((resolve, reject) => {
@@ -16,52 +18,27 @@ function main() {
       request.onerror = () => reject('Oops... Something went wrong!')
       request.send()
     })
-
   }
 
   function calculateInput() {
     const currency = document.querySelector('.currency-symbol').textContent
-    const resultSymbol = document.querySelector('.result-symbol').textContent
 
-    let url
+    let currencyString, type
 
     if (currency !== 'BTC') {
-      url = `https://api.coindesk.com/v1/bpi/currentprice/${currency}.json`
+      currencyString = currency
+      type = nonCryptocurrency
     } else {
-      url = `https://api.coindesk.com/v1/bpi/currentprice/${resultSymbol}.json`
+      currencyString = document.querySelector('.result-symbol').textContent
+      type = cryptocurrency
     }
 
-    const inputCurrency = document.querySelector('#input-currency')
-    const outputCurrency = document.querySelector('.result-content')
+    const url = `https://api.coindesk.com/v1/bpi/currentprice/${currencyString}.json`
 
     requestAPI('GET', url)
       .then(response => {
-        let worth
-
-        if (currency !== 'BTC') {
-          worth = response.bpi[`${currency}`].rate
-        } else {
-          worth = response.bpi[`${resultSymbol}`].rate
-        }
-
-        let output
-
-        if (currency !== 'BTC') {
-          output = inputCurrency.value / parseFloat(worth.replace(',', ''))
-        } else {
-          output = inputCurrency.value * parseFloat(worth.replace(',', ''))
-        }
-
-        if (currency !== 'BTC') {
-          if (output.toString().length > 7) outputCurrency.textContent = output.toFixed(7)
-          else outputCurrency.textContent = output
-        } else {
-          if (output === 0) {
-            outputCurrency.textContent = output
-          } else {
-            outputCurrency.textContent = output.toFixed(2)
-          }
-        }
+        const currencyType = currencyStrategy(type, response, currencyString)
+        currencyType.calculate()
       })
       .catch(error => {
         console.error(error)
