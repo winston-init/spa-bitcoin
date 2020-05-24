cf7c35cb95e5b72e9b2ea76eb1f4fc192def5b8f
winston <winston.init@gmail.com>
2020-05-24 17:46:51 -0300

    remove class from navbar mobile action

--- src/views/pages/SentMessage.js
+++ src/views/pages/SentMessage.js
@@ -1,7 +1,7 @@
 const MessageSent = {
   render: async () => {
     const view = /* html */ `
-      <h2>Hello person, your message was sent with sucess. So as possible, we'll send you an email 
+      <h2 class="message">Hello person, your message was sent with sucess. So as possible, we'll send you an email 
       to person@email.com with the answer.</h2>
     `
 
@@ -9,6 +9,7 @@ const MessageSent = {
   },
   afterRender: async () => {
     document.querySelector('footer').classList.add('fixed-in-bottom')
+    document.querySelector('body').classList.remove('blocked')
    }
 }
 
